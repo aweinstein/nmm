204d7a75a7fcda3294824587859eef07cfca98cd
Patricio Orio <patricio.orio@uv.cl>
2015-04-27 13:08:38 -0300

    Agregar conexiones

--- nmmw.py
+++ nmmw.py
@@ -40,6 +40,30 @@ def f(y, t, C=68):
     y5_dot = B * b * (C4 * s(C3 * y0)) - 2 * b * y5 - b**2 * y2
     return np.array((y0_dot, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot))
 
+def f2(y, t, C=68):
+    y0, y1, y2, y3, y4, y5 = y
+    #p = 220 + 22 * np.random.randn() 
+    #Page 4 of Jansen et al, "p(t) will have
+    # an amplitude varying between 120 and 320 pulses per second."
+    p = 120 + 200 * np.random.rand() 
+    A = 3.25
+    B = 22
+    a = 100
+    b = 50
+    C1 = C
+    C2 = 0.8 * C
+    C3 = 0.25 * C
+    C4 = 0.25 * C
+
+    y0_dot = y3
+    y3_dot = A * a * s(C2*y1 - C4*y2) - 2 * a *y3 - a**2 * y0
+    y1_dot = y4
+    y4_dot = A * a * (p/C2 + s(C1 * y0)) - 2 *a * y4 - a**2 * y1
+    y2_dot = y5
+    y5_dot = B * b * s(C3 * y0) - 2 * b * y5 - b**2 * y2
+    return np.array((y0_dot, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot))
+
+
 def plot_sigmoid():
     v = np.linspace(-1, 10)
     plt.plot(v, s(v))
@@ -52,13 +76,13 @@ if __name__ == '__main__':
     for C in Cs:
         # Run it once for a brief time to get sensible initial conditions
         ic = 6 * (0,)
-        sol, t = ode_euler(f, ic, 0.3, 1e-3, params=(C,))
+        sol, t = ode_euler(f2, ic, 0.3, 1e-3, params=(C,))
         ic = sol[-1,:]
         # Run it again with the new initial conditions
-        sol, t = ode_euler(f, ic, 2, 1e-3, params=(C,))
+        sol, t = ode_euler(f2, ic, 2, 1e-3, params=(C,))
 
         y0, y1, y2 = sol[:,:3].T
-        EEGs.append(y1 - y2)
+        EEGs.append(C2*y1 - C4*y2)
 
 
     # f, axs = plt.subplots(6, figsize=(21,8))
@@ -71,7 +95,7 @@ if __name__ == '__main__':
     # plt.plot(t, y1 - y2)
 
     plt.close('all')
-    f, axs = plt.subplots(len(EEGs), figsize=(21,10))
+    _, axs = plt.subplots(len(EEGs), figsize=(21,10))
     for EEG, ax, C in zip(EEGs, axs, Cs):
         ax.plot(t, EEG)
         #ax.set_xticks([])
@@ -83,3 +107,4 @@ if __name__ == '__main__':
 
         
 
+1
--- nmmwMulti.py
+++ nmmwMulti.py
@@ -12,11 +12,11 @@ from ode_eulermulti import ode_eulerM
 import matplotlib.pyplot as plt
 
 # Parameters
-A = 3.25,3.22,3.28
+A = 3.25
 B = 22
 a = 100
 b = 50
-C = 150
+C = 270
 C1 = C
 C2 = 0.8 * C
 C3 = 0.25 * C
@@ -24,6 +24,8 @@ C4 = 0.25 * C
 
 M = 220  #Input mean
 D = 22   #Input variance
+
+
 
 
 def s(v, e0 = 2.5, r = 0.56, v0 = 6):
@@ -42,6 +44,19 @@ def f(y, t):
     y5_dot = B * b * (C4 * s(C3 * y0)) - 2 * b * y5 - b**2 * y2
     return np.array((y0_dot, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot))
 
+def f2(y, t):
+    y0, y1, y2, y3, y4, y5 = y
+    
+    p = 220 + 100 * np.random.randn()
+    y0_dot = y3
+    y3_dot = A * a * s(C2 * y1 - C4 * y2) - 2 * a *y3 - a**2 * y0
+    y1_dot = y4
+    y4_dot = A * a * (p/C2 + s(C1 * y0) + np.dot(CMat,s(C2 * y1 - C4 * y2))) - 2 *a * y4 - a**2 * y1
+    y2_dot = y5
+    y5_dot = B * b * s(C3 * y0) - 2 * b * y5 - b**2 * y2
+    return np.array((y0_dot, y1_dot, y2_dot, y3_dot, y4_dot, y5_dot))
+
+
 def plot_sigmoid():
     v = np.linspace(-1, 10)
     plt.plot(v, s(v))
@@ -50,6 +65,14 @@ def plot_sigmoid():
 if __name__ == '__main__':
     np.random.seed(1234)
     nsim=50   
+    
+    CMat=np.zeros((nsim,nsim))
+    for i in range(nsim):
+        CMat[i,i-1]=0.5
+        if np.random.uniform()>0.8:
+            CMat[i,i-20]=0.5
+    
+    
     t0=time.time()
     A=np.linspace(3,4,nsim)
 #    t = np.linspace(0, 1, 10000)
@@ -60,21 +83,24 @@ if __name__ == '__main__':
     ic = ic.T     
 
     t0=time.time()    
-    sol, t = ode_eulerM(f, ic, 25, 1e-3)
+#    sol, t = ode_eulerM(f, ic, 5, 1e-3)
+    sol2, t = ode_eulerM(f2, ic, 5, 1e-3)
     print time.time()-t0    
     
-    v1 = sol[:,3]
-    v2 = sol[:,4]
-    y = v1 - v2
+#    v1 = sol[:,1]
+#    v2 = sol[:,2]
+#    y = v1 - v2
+    y2 = C2*sol2[:,1] - C4*sol2[:,2]
     plt.figure(1)
     plt.clf()
-    plt.plot(t, y)
+#    plt.plot(t[1000:], y[1000:])
+    plt.plot(t[1000:], y2[1000:])
     
     plt.figure(2)    
     plt.clf()
     for i in range(6):
         plt.subplot(7,1,i+1)
-        plt.plot(sol[:,i])
+        plt.plot(sol2[:,i])
     plt.subplot(7,1,7)
 #    plt.plot(P)
     
